2a6b83a8b2130f8cabf2181f91488c3baac5182b
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-11-24 13:06:07 +0700

    setpup deploy

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,15 +1,18 @@
 const { Sequelize } = require("sequelize");
 const postgresConfig = require("../configs/database");
 
-const sequelize = new Sequelize({
-  username: postgresConfig.DB_USERNAME,
-  password: postgresConfig.DB_PASSWORD,
-  database: postgresConfig.DB_NAME,
-  port: 5432,
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+// local connect database
+// const sequelize = new Sequelize({
+//   username: postgresConfig.DB_USERNAME,
+//   password: postgresConfig.DB_PASSWORD,
+//   database: postgresConfig.DB_NAME,
+//   port: 5432,
+//   host: "localhost",
+//   dialect: "postgres",
+//   logging: false,
+// });
+
+const sequelize = new Sequelize(process.env.DB_URL);
 
 const User = require("../models/user")(sequelize);
 const PhotoProfile = require("../models/photo_profile")(sequelize);
