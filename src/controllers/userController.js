342d2d93cc731fd770f61cc8014486a5dcc472ee
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-11-28 17:05:26 +0700

    fix response user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,7 +28,7 @@ const userController = {
       });
     } catch (err) {
       return res.status(err.statusCode || 500).json({
-        message: err.message,
+        data: err.message,
       });
     }
   },
